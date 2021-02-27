ab017d14ae78ffd3990834ecbe19d77840c41c82
Alvin Huang <alvinhuang571@gmail.com>
2021-02-27 16:49:40 -0500

    add firebase

---
+++ Code_Project/firebase.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBaR8V38npOrSikrz63vXC-lyYCnshS-0U",
+    authDomain: "webapp123-eb252.firebaseapp.com",
+    projectId: "webapp123-eb252",
+    storageBucket: "webapp123-eb252.appspot.com",
+    messagingSenderId: "471504503340",
+    appId: "1:471504503340:web:967a39338370714fcb8789",
+    measurementId: "G-3GX6EDF5WY"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
